3490a4bbb1ce1196d980675acb66ce9b5e5ef8c7
vijayanandraj@gmail.com <Vinisha1976$>
2023-01-26 16:45:36 +0530

    updated view classes

--- movie_api/urls.py
+++ movie_api/urls.py
@@ -1,7 +1,11 @@
-from movie_api.views import MovieViewSet
+from movie_api.views import MovieList, MovieDetail
 from rest_framework.routers import DefaultRouter
 from movie_api import views
+from django.urls import path
 
-router = DefaultRouter()
-router.register(r'movies', views.MovieViewSet, basename='movie')
-urlpatterns = router.urls
+#router = DefaultRouter()
+#router.register(r'movies', views.MovieViewSet, basename='movie')
+urlpatterns = [
+    path("movies/<int:pk>/", MovieDetail.as_view(), name="movie_detail"),
+    path("movies/", MovieList.as_view(), name="movie_list")
+]
--- movie_api/views.py
+++ movie_api/views.py
@@ -4,9 +4,15 @@ from django.shortcuts import render
 from django.shortcuts import render
 from .models import Movie
 from .serializer import MovieSerializer
-from rest_framework import viewsets
+from rest_framework import generics
 
 # Create your views here.
-class MovieViewSet(viewsets.ModelViewSet):
-    serializer_class = MovieSerializer
+class MovieList(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
